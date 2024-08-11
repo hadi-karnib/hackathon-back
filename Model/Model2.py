4843072094c0acb722f75962db1e47fc802e874a
Jason Greich <jasongreich@gmail.com>
2024-08-11 18:12:15 +0300

    registered three partitins of model 2

--- Model/Model2.py
+++ Model/Model2.py
@@ -8,11 +8,27 @@ import pandas as pd  # for working with DataFrames
 import matplotlib.pyplot as plt  # for data visualization
 import json
 from sklearn.preprocessing import LabelEncoder
-from sklearn import tree
+
+import numpy as np
+import mlflow
+import mlflow.sklearn
+
+
+def mlFlowVersioning(model,model_name):
+    with mlflow.start_run() as run:
+        # Log the model
+        mlflow.sklearn.log_model(model, "model1")
+        
+        # Register the model
+        model_uri = f"runs:/{run.info.run_id}/model1"
+        mlflow.register_model(model_uri, model_name)
+
 
 def train_model_partition1():
     # load dataset
-    df = pd.read_csv('data/first_data.csv') # app, category, size, type, price, content rating, genre last updated, android version
+    df = pd.read_csv('data/first_data.csv') # App,Category,Installs_category,Rating,Mean_App_Sentiment,Reviews,Size_in_MB,Type,Price,Content_Rating,Genres,Last_Updated,Android_Ver
+    
+    # Encoding categorical features
     label_encoder = LabelEncoder()
     df['Category'] = label_encoder.fit_transform(df['Category'])
     df['Content_Rating'] = label_encoder.fit_transform(df['Content_Rating'])
@@ -20,8 +36,9 @@ def train_model_partition1():
     df['Type'] = label_encoder.fit_transform(df['Type'])
     df['Android_Ver'] = label_encoder.fit_transform(df['Android_Ver'])
     df['Installs_category'] = label_encoder.fit_transform(df['Installs_category'])
+
     # choose features and label
-    x = df.drop(['Installs_category', 'App', 'Last_Updated', 'Rating', 'Mean_App_Sentiment', 'Reviews'], axis=1)  # Features
+    x = df.drop(['Installs_category', 'App', 'Last_Updated', 'Mean_App_Sentiment'], axis=1)  # Features
     y = df['Installs_category']  # Target
 
     # Splitting the dataset into training and other sets (80/20)
@@ -30,22 +47,32 @@ def train_model_partition1():
     # Splitting the dataset into testing and validation sets (50/50)
     x_test, x_validate, y_test, y_validate = train_test_split(x_other, y_other, test_size=0.5, random_state=28)
 
-    class_tree = tree.DecisionTreeClassifier(criterion= 'entropy', max_depth= 5, min_samples_leaf= 2, min_samples_split= 2)
-    class_tree.fit(x_train, y_train)
+    # Initializing the Random Forest Regression model with 10 decision trees
+    model = RandomForestRegressor(n_estimators=20, min_samples_leaf=5, random_state=28)
+    
+    
+    # Fitting the Random Forest Regression model to the data
+    model.fit(x_train, y_train)
 
-    y_pred = class_tree.predict(x_test)
-
-    score = class_tree.score(x_test, y_test)
+    score = model.score(x_test, y_test)
     print(score)
 
+    y_pred = model.predict(x_test)
+    #RMSE (Root Mean Square Error)
     rmse = float(format(np.sqrt(mean_squared_error(y_test, y_pred)), '.3f'))
     print("\nRMSE:\n", rmse)
+    mlFlowVersioning(model,"model2-partition1")
 
-    return class_tree
+    return model
+
+modell2_partition1=train_model_partition1()
+
 
 def train_model_partition2():
     # load dataset
-    df = pd.read_csv('data/second_data.csv') # app, category, size, type, price, content rating, genre last updated, android version
+    df = pd.read_csv('data/second_data.csv')
+
+    # Encoding categorical features
     label_encoder = LabelEncoder()
     df['Category'] = label_encoder.fit_transform(df['Category'])
     df['Content_Rating'] = label_encoder.fit_transform(df['Content_Rating'])
@@ -53,8 +80,9 @@ def train_model_partition2():
     df['Type'] = label_encoder.fit_transform(df['Type'])
     df['Android_Ver'] = label_encoder.fit_transform(df['Android_Ver'])
     df['Installs_category'] = label_encoder.fit_transform(df['Installs_category'])
+
     # choose features and label
-    x = df.drop(['Installs_category', 'App', 'Last_Updated', 'Rating', 'Mean_App_Sentiment', 'Reviews'], axis=1)  # Features
+    x = df.drop(['Installs_category', 'App', 'Last_Updated', 'Mean_App_Sentiment'], axis=1)  # Features
     y = df['Installs_category']  # Target
 
     # Splitting the dataset into training and other sets (80/20)
@@ -63,22 +91,29 @@ def train_model_partition2():
     # Splitting the dataset into testing and validation sets (50/50)
     x_test, x_validate, y_test, y_validate = train_test_split(x_other, y_other, test_size=0.5, random_state=28)
 
-    class_tree = tree.DecisionTreeClassifier(criterion= 'gini', max_depth= 10, min_samples_leaf= 4, min_samples_split= 10)
-    class_tree.fit(x_train, y_train)
+    # Initializing the Random Forest Regression model with 10 decision trees
+    model = RandomForestRegressor(n_estimators=20, min_samples_split=5, random_state=28)
+    
+    # Fitting the Random Forest Regression model to the data
+    model.fit(x_train, y_train)
 
-    y_pred = class_tree.predict(x_test)
-
-    score = class_tree.score(x_test, y_test)
+    score = model.score(x_test, y_test)
     print(score)
 
+    y_pred = model.predict(x_test)
+    #RMSE (Root Mean Square Error)
     rmse = float(format(np.sqrt(mean_squared_error(y_test, y_pred)), '.3f'))
     print("\nRMSE:\n", rmse)
-
-    return class_tree
+    
+    mlFlowVersioning(model,"model2-partition2")
+    return model
+modell2_partition2=train_model_partition2()
 
 def train_model_partition3():
     # load dataset
-    df = pd.read_csv('data/finaldataset.csv') # app, category, size, type, price, content rating, genre last updated, android version
+    df = pd.read_csv('data/finaldataset.csv')
+
+    # Encoding categorical features
     label_encoder = LabelEncoder()
     df['Category'] = label_encoder.fit_transform(df['Category'])
     df['Content_Rating'] = label_encoder.fit_transform(df['Content_Rating'])
@@ -86,8 +121,9 @@ def train_model_partition3():
     df['Type'] = label_encoder.fit_transform(df['Type'])
     df['Android_Ver'] = label_encoder.fit_transform(df['Android_Ver'])
     df['Installs_category'] = label_encoder.fit_transform(df['Installs_category'])
+
     # choose features and label
-    x = df.drop(['Installs_category', 'App', 'Last_Updated', 'Rating', 'Mean_App_Sentiment', 'Reviews'], axis=1)  # Features
+    x = df.drop(['Installs_category', 'App', 'Last_Updated', 'Mean_App_Sentiment'], axis=1)  # Features
     y = df['Installs_category']  # Target
 
     # Splitting the dataset into training and other sets (80/20)
@@ -96,26 +132,31 @@ def train_model_partition3():
     # Splitting the dataset into testing and validation sets (50/50)
     x_test, x_validate, y_test, y_validate = train_test_split(x_other, y_other, test_size=0.5, random_state=28)
 
-    class_tree = tree.DecisionTreeClassifier(criterion= 'gini', max_depth= 10, min_samples_leaf= 2, min_samples_split= 5)
-    class_tree.fit(x_train, y_train)
+    # Initializing the Random Forest Regression model with 10 decision trees
+    model = RandomForestRegressor(n_estimators=20, min_samples_split=5, random_state=28)
+    
+    # Fitting the Random Forest Regression model to the data
+    model.fit(x_train, y_train)
 
-    y_pred = class_tree.predict(x_test)
-
-    score = class_tree.score(x_test, y_test)
+    score = model.score(x_test, y_test)
     print(score)
 
+    y_pred = model.predict(x_test)
+    #RMSE (Root Mean Square Error)
     rmse = float(format(np.sqrt(mean_squared_error(y_test, y_pred)), '.3f'))
     print("\nRMSE:\n", rmse)
+    mlFlowVersioning(model,"model2-partition3")
+    return model
 
-    return class_tree
-
+modell2_partition3=train_model_partition3()
 
 def predict_rate(model, user_input_json):
     # Load the JSON file into a DataFrame
-    #with open(user_input_json, 'r') as f:
-    #    data = [json.load(f)]
-    user_input = pd.read_json(user_input_json)
-    
+    with open(user_input_json, 'r') as f:
+        data = [json.load(f)]
+    user_input = pd.DataFrame(data)
+
+    # Encoding categorical features
     label_encoder = LabelEncoder()
     user_input.drop(['App', 'Last_Updated'], axis=1) 
     user_input['Category'] = label_encoder.fit_transform(user_input['Category'])
@@ -124,9 +165,12 @@ def predict_rate(model, user_input_json):
     user_input['Type'] = label_encoder.fit_transform(user_input['Type'])
     user_input['Android_Ver'] = label_encoder.fit_transform(user_input['Android_Ver'])
     x = user_input.drop(['App', 'Last_Updated'], axis=1)
+
+    # Predicting the installs
     value = model.predict(x)
-    value = round(value[0], 0)
+
     # The installs category
+    value = round(value[0], 0)
     if value<=0:
         result = 'no'
         installs = '0'
@@ -158,9 +202,7 @@ def predict_rate(model, user_input_json):
         "Installs_category": result,
         "Installs": installs
     }
-    #file_name = 'data.json'
-    
-    # Save the data to a JSON file
-    #with open(file_name, 'w') as json_file:
-    #    json.dump(data, json_file, indent=4)
-    return data
+    file_name = 'data.json'
+    with open(file_name, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+    return file_name
